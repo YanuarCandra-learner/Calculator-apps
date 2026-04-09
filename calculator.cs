2717f7f282e6747f5f459cbb55b5b414e1577cd7
Yanuar Adi Candra <y.adi.candra@students.amikom.ac.id>
2026-04-09 10:15:41 +0700

    added plus minus on calculator by integer variable

--- calculator.cs
+++ calculator.cs
@@ -1,8 +1,25 @@
 class calculator
 {
-static void Main(string[] args)
-{
-	
-}
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 6;
+
+        Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+        Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, pengurangan(a, b));
+
+        Console.WriteLine("\nTekan Sembarang Key untuk keluar");
+        Console.ReadKey();
+    }
+
+    static int penambahan(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int pengurangan(int a, int b)
+    {
+        return a - b;
+    }
 }
 	
